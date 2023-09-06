5c39762327fabe0b022ed708c5f024fd5e9dfe04
Reece Harris <reeceharris@email.com>
2023-09-06 11:55:52 +0100

    Add jsDoc to hash mixer

--- src/mixer/hashing.js
+++ src/mixer/hashing.js
@@ -5,6 +5,17 @@ const supported_algos = Object.fromEntries(
 );
 const supported_algos_keys = Object.keys(supported_algos);
 
+/**
+ * Calculate multiple hash values for a given data using specified hashing algorithms.
+ * @param {string[]} algos - An array of hashing algorithm names to be used in order.
+ * @param {string|Buffer} data - The data to be hashed using the specified algorithms.
+ * @param {string|null} [salt=null] - An optional salt to add to the data before hashing (default is null).
+ * @param {string} [encoding='hex'] - The encoding format for the final hash result (default is 'hex').
+ * @returns {string} The final hash value obtained by applying the specified hashing algorithms sequentially.
+ * @throws {Error} If the 'algos' parameter is not an array or contains less than 2 supported hashing algorithm names.
+ * @throws {Error} If any of the specified hashing algorithms in 'algos' are not supported.
+ * @throws {Error} If 'data' is undefined or null.
+*/
 module.exports = (algos=[], data, salt=null, encoding='hex') => {
 
 	if (!Array.isArray(algos)) {
